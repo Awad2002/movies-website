702233d992f7b16aa9b7acc7e6ecb55f18a6e950
Amr Awad <awadamr33@gmail.com>
2022-01-13 16:18:39 +0200

    asdsadad

--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -21,7 +21,6 @@ export default {
                 .then(response => response.json())
                 .then(data => {
                     movie.value = data
-                    console.log(data);
                 });
         });
 
